42c641512934367de6e5b96dfc609ba6758830b4
WhiteArct1c <matrodrigues1576@gmail.com>
2023-08-17 02:07:46 -0300

    feat: initial commit

---
+++ src/theme/globalStyles.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyle = createGlobalStyle`
+   body{
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+
+      background-color: #EFF2F6;
+   }
+`
