e7ba843bb249c8cc9202f434c2057edcccb06b3d
WhiteArct1c <matrodrigues1576@gmail.com>
2023-08-17 05:34:03 -0300

    fix: adjusting global styles

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -2,11 +2,15 @@ import { createGlobalStyle } from "styled-components";
 
 
 export const GlobalStyle = createGlobalStyle`
+
    body{
       padding: 0;
       margin: 0;
       box-sizing: border-box;
 
       background-color: #EFF2F6;
+
+      font-family: 'Public Sans', sans-serif;
+      font-weight: 400;
    }
 `
