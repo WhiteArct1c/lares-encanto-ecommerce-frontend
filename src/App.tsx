ddf7d955c433f794363848534df4841f2e1632fc
WhiteArct1c <matrodrigues1576@gmail.com>
2023-08-17 05:35:55 -0300

    feat: adding navbar and footer on the main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-function App() {
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
+import NavBar from "./shared/NavBar"
+import Footer from "./shared/Footer"
 
+function App() {
   return (
     <>
-     
+     <Grid2 container rowSpacing={5}>
+        <Grid2 xs={12}>
+          <NavBar isAdmin={false}/>
+        </Grid2>
+        <Grid2 xs={12}>
+          CONTEUDO VAI FICAR AQUI
+        </Grid2>
+        <Grid2 xs={12}>
+          <Footer/>
+        </Grid2>
+     </Grid2>
     </>
   )
 }
